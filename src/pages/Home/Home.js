fac95caae1364d3b54adf9e18486eda8b4d23553
ofkarakus <omerfarukkarakus01@gmail.com>
2020-12-08 21:37:06 +0000

    feat: fetch posts from firestore (realtime)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 import { PostCard, StatusBar } from "../../components/";
+import firebase from "../../firebase/firebase.utils";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,14 +17,38 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export function Home() {
+  const [posts, setPosts] = useState();
+
+  const fetchPosts = () => {
+    const doc = firebase.firestore.collection("posts");
+    doc.onSnapshot(
+      (snapShot) => {
+        setPosts(snapShot.docs.map((doc) => doc.data()));
+      },
+      (err) => {
+        console.log(`Encountered error: ${err}`);
+      }
+    );
+  };
+
+  useEffect(fetchPosts, []);
+
   const classes = useStyles();
 
   return (
     <Container className={classes.root}>
       <div>
         {/* <StatusBar /> */}
-        <Grid container spacing={2} className={classes.gridContainer}>
-          <PostCard />
+        <Grid container spacing={8} className={classes.gridContainer}>
+          {posts?.map((post, index) => (
+            <Grid key={index} item xs={12}>
+              <PostCard
+                userName={post.userName}
+                caption={post.caption}
+                fileUrl={post.fileUrl}
+              />
+            </Grid>
+          ))}
         </Grid>
       </div>
       <div></div>
