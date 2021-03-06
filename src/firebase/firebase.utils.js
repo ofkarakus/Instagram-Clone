1cfe608c05aca4b1787e6130a10e5b8b8d32903a
Omer Karakus <omerfarukkarakus01@gmail.com>
2021-03-06 16:10:37 +0000

    add production configs

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -19,10 +19,18 @@ const developmentConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-const productionConfig = {};
+const productionConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
 const firebaseConfig =
-  process.env.NODE_ENV == "development" ? developmentConfig : productionConfig;
+  process.env.NODE_ENV === "development" ? developmentConfig : productionConfig;
 
 // Initialize Firebase
 class Firebase {
